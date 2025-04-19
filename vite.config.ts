7c9409a112707556ebabce6e89f69c6da4bf7d2d
yashmasalge <myash2101@gmail.com>
2025-04-19 14:12:52 +0530

    pages config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/Portfolio_react_vite/',
   server: {
     host: "::",
     port: 8080,
